f7752f6353f97740195a45c3d74a3f8c13b44a18
Mohammedinzamam <165227203+Mohammedinzamam@users.noreply.github.com>
2024-04-20 13:53:43 +0000

    commit

--- Hello.py
+++ Hello.py
@@ -1,51 +1,41 @@
-# Copyright (c) Streamlit Inc. (2018-2022) Snowflake Inc. (2022)
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 import streamlit as st
-from streamlit.logger import get_logger
+def main():
+    st.title("Event Showcase")
 
-LOGGER = get_logger(__name__)
+    st.subheader("Events")
+    img=[['/workspaces/event/image/eveimage.webp', '/workspaces/event/image/eveimage.webp','/workspaces/event/image/eveimage.webp','/workspaces/event/image/eveimage.webp'],['/workspaces/event/image/eveimage.webp', '/workspaces/event/image/eveimage.webp','/workspaces/event/image/eveimage.webp','/workspaces/event/image/eveimage.webp'],['/workspaces/event/image/eveimage.webp', '/workspaces/event/image/eveimage.webp','/workspaces/event/image/eveimage.webp','/workspaces/event/image/eveimage.webp'],['/workspaces/event/image/eveimage.webp', '/workspaces/event/image/eveimage.webp','/workspaces/event/image/eveimage.webp','/workspaces/event/image/eveimage.webp']]
+    Date=[['2024-04-20', '2024-04-21', '2024-04-22', '2024-04-23'],['2024-04-20', '2024-04-21', '2024-04-22', '2024-04-23'],['2024-04-20', '2024-04-21', '2024-04-22', '2024-04-23'],['2024-04-20', '2024-04-21', '2024-04-22', '2024-04-23']]
+    loc=[['Location A', 'Location B','Location C', 'Location D'],['Location A', 'Location B','Location C', 'Location D'],['Location A', 'Location B','Location C', 'Location D'],['Location A', 'Location B','Location C', 'Location D']]
+    c=1
+    j=0
+    for f in range(4):
+            col1, col2,col3,col4,col5=st.columns(5)
+            with col1:
+                st.image(img[f][0], caption=f"Date: {Date[f][0]}, Location: {loc[f][0]}", width=120)
+            with col2:
+                st.image(img[f][1], caption=f"Date: {Date[f][1]}, Location: {loc[f][1]}", width=120)
+            with col3:
+                st.image(img[f][2], caption=f"Date: {Date[f][2]}, Location: {loc[f][2]}", width=120)
+            with col4:
+                st.image(img[f][3], caption=f"Date: {Date[f][3]}, Location: {loc[f][3]}", width=120)  
+            with col5:
+                if f==0:
+                    if st.button("View More", key="home_button"):
+                        st.write("hello")
+                elif f==1:
+                    if st.button("View More", key="home_button2"):
+                        st.markdown("[Home Page](http://localhost:8501/home)")
+                elif f==2:
+                    if st.button("View More", key="home_button3"):
+                        st.markdown("[Home Page](http://localhost:8501/home)")
+                elif f==3:
+                    if st.button("View More", key="home_button4"):
+                        st.markdown("[Home Page](http://localhost:8501/home)")
 
+                
 
-def run():
-    st.set_page_config(
-        page_title="Hello",
-        page_icon="👋",
-    )
-
-    st.write("# Welcome to Streamlit! 👋")
-
-    st.sidebar.success("Select a demo above.")
-
-    st.markdown(
-        """
-        Streamlit is an open-source app framework built specifically for
-        Machine Learning and Data Science projects.
-        **👈 Select a demo from the sidebar** to see some examples
-        of what Streamlit can do!
-        ### Want to learn more?
-        - Check out [streamlit.io](https://streamlit.io)
-        - Jump into our [documentation](https://docs.streamlit.io)
-        - Ask a question in our [community
-          forums](https://discuss.streamlit.io)
-        ### See more complex demos
-        - Use a neural net to [analyze the Udacity Self-driving Car Image
-          Dataset](https://github.com/streamlit/demo-self-driving)
-        - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
-    """
-    )
 
 
 if __name__ == "__main__":
-    run()
+    main()
+
