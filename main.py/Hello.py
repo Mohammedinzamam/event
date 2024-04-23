3aecdca655d9536ae434632cf691e68117eec792
Mohammedinzamam <165227203+Mohammedinzamam@users.noreply.github.com>
2024-04-23 13:45:45 +0000

    commit

--- main.py/Hello.py
+++ main.py/Hello.py
@@ -21,17 +21,16 @@ def main():
             with col5:
                 if f==0:
                     if st.button("View More", key="home_button"):
-                        st.switch_page("/workspaces/event/main/page1.py")
+                        st.markdown("[Home Page](http://localhost:8501//workspaces/event/main.py/Hello.py)")
                 elif f==1:
                     if st.button("View More", key="home_button2"):
-                        st.switch_page("/workspaces/event/page2.py")
+                        st.markdown("[Home Page](http://localhost:8501//workspaces/event/main.py/page2.py)")
                 elif f==2:
                     if st.button("View More", key="home_button3"):
-                        st.markdown("[Home Page](http://localhost:8501/home)")
+                        st.markdown("[Home Page](/workspaces/event/main.py/page2.py)")
                 elif f==3:
                     if st.button("View More", key="home_button4"):
-                        st.markdown("[Home Page](http://localhost:8501/home)")
-
+                        st.markdown("[Home Page](http://localhost:8501/main.py/page2.py)")
                 
 
 
--- main.py/page2.py
+++ main.py/page2.py
@@ -1,6 +1,1 @@
 import streamlit as st
-def main():
-    st.title("Event Showcase")
-
-if __name__ == "__main__":
-    main()
